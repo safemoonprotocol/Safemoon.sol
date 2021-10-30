b69854c65484f4c010050937d177c59abafa6974
Pronabjyoti borgohain <79841399+Pjb1994-1@users.noreply.github.com>
2021-10-31 00:37:19 +0530

    Update safmoon.sol

--- Safemoon.sol
+++ Safemoon.sol
@@ -12,7 +12,7 @@
    
    #LIQ+#RFI+#SHIB+#DOGE = #BEE
 
-   #SAFEMOON features:
+   #DIGIMART features:
    3% fee auto add to the liquidity pool to locked forever when selling
    2% fee auto distribute to all holders
    I created a black hole so #Bee token will deflate itself in supply with every transaction
