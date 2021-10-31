36608ea38708f87b518b619982d3e565eb4fc967
Pronabjyoti borgohain <79841399+Pjb1994-1@users.noreply.github.com>
2021-10-31 18:48:15 +0530

    Revert "Update safmoon.sol"

--- DigiMart.sol
+++ DigiMart.sol
@@ -701,7 +701,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
 }
 
 
-contract DigiMart is Context, IERC20, Ownable {
+contract SafeMoon is Context, IERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
 
@@ -719,9 +719,9 @@ contract DigiMart is Context, IERC20, Ownable {
     uint256 private _rTotal = (MAX - (MAX % _tTotal));
     uint256 private _tFeeTotal;
 
-    string private _name = "DigiMart";
-    string private _symbol = "DMART";
-    uint8 private _decimals = 18;
+    string private _name = "SafeMoon";
+    string private _symbol = "SAFEMOON";
+    uint8 private _decimals = 9;
     
     uint256 public _taxFee = 5;
     uint256 private _previousTaxFee = _taxFee;
